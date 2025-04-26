d38dde0e8130f27fd7ce5230114ef7ca8580c01b
Fabiang120 <117304307+Fabiang120@users.noreply.github.com>
2025-04-25 22:01:16 -0400

    Started page class

--- storage.h
+++ storage.h
@@ -4,9 +4,29 @@
 
 #ifndef MYDATABASE_STORAGE_H
 #define MYDATABASE_STORAGE_H
-
+#include <iostream>
+#include <vector>
+#include <cstdint>
 
 class storage {
+public:
+    class Page{
+    public:
+        static const std::size_t PAGE_SIZE = 4096;
+        std::vector<uint8_t> data;
+        int id;
+        bool dirty;
+        int pincount;
+        Page(){
+            id = -1;
+            data.assign(PAGE_SIZE, 0);
+            dirty = false;
+            pincount = 0;
+        }
+
+    };
+
+
 
 };
 
